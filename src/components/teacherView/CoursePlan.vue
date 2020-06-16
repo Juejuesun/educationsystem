5b4c1dc1336666e0b092ce85896e153d40475f87
Juejuesun <48380934+Juejuesun@users.noreply.github.com>
2020-06-16 17:26:14 +0800

    添加授课计划

--- src/components/teacherView/CoursePlan.vue
+++ src/components/teacherView/CoursePlan.vue
@@ -2,17 +2,18 @@
     <div id="app">
         <h3>授课计划</h3>
         <el-divider></el-divider>
-        <el-button @click="addVisible = true">添加</el-button>
+        <el-button @click="addVisible = true"><i class="fa fa-pencil"></i>添加</el-button>
         <vue-event-calendar
         :events="demoEvents"
         title="授课计划"
         @day-changed="handleDayChanged">
         <template scope="props">
-            <el-button  @click="addVisible = true">添加</el-button>
+            <!-- <el-button  @click="addVisible = true">添加</el-button> -->
+            <el-button  @click="addVisible = true" style="margin: 0; padding: 8px 20px 8px 20px;"><i class="fa fa-pencil-square-o addBoxpen" aria-hidden="true"></i></el-button>
             <div v-for="(event, index) in props.showEvents" :key="index" class="event-item" @mouseenter="event.isShow = true" @mouseleave="event.isShow = false">
                 <div class="titleBox">
                     <div>{{index+1}}.{{event.title}}</div>
-                    <div>{{event.date}}<i @click="delPlan(event, index)" v-show="event.isShow" class="el-icon-delete icoBtn"></i></div>
+                    <div>{{event.date}}<i @click="delPlan(event, index)" v-show="event.isShow" class="el-icon-delete icoBtn animated fadeIn"></i></div>
                 </div>
                 <hr style="FILTER:alpha(opacity=100,finishopacity=0,style=3)" width="95%" color=#E4E7ED SIZE=1/>
                 <div>
@@ -49,9 +50,13 @@
 </template>
 
 <script>
-let today = new Date()
+import {mapState} from 'vuex'
+
 export default {
   name: 'app',
+  computed: {
+    ...mapState(['userInfo'])
+  },
   data () {
     return {
         addVisible: false,
@@ -78,6 +83,9 @@ export default {
       }]
     }
   },
+  created() {
+    this.creMeno()
+  },
   methods: {
     handleDayChanged (data) {
       console.log('date-changed', data)
@@ -88,22 +96,100 @@ export default {
     // handleMonthChanged (data) {
     //   console.log('month-changed', data)
     // },
-    addPlan() {
-        // console.log(this.form.date)
-        let asc = {
-            date: this.form.date,
-            title: this.form.title,
-            desc: this.form.desc,
-            isShow: false
-        }
-        this.demoEvents.splice(0,0,asc)
-        this.addVisible = false
+    async creMeno() {
+      let newArr = await this.getMemo()//更新
+      this.demoEvents = JSON.parse(JSON.stringify(newArr))
+    },
+    async getMemo() {
+      let asc = {
+        teacherId: this.userInfo.userId
+      }
+      const {data: res} = await this.$http.post('/teacher/getMemo', asc)
+      // console.log(res)
+      let aa = this.strToArrChange(res.teacherMemo)//解析
+      // console.log('获取',aa)
+      return aa
+    },
+    async setMemo(arr) {
+      let temp = JSON.parse(JSON.stringify(arr))
+      let ss = this.arrToStrChange(temp)//转换
+      let asc = {
+        teacherId: this.userInfo.userId,
+        teacherMemo: ss
+      }
+      const {data: res} = await this.$http.post('/teacher/setMemo', asc)
+      console.log(res)
+      if(res.status == 'success') {
+        let newArr = await this.getMemo()//更新
+        this.demoEvents = JSON.parse(JSON.stringify(newArr))
+        return res
+      }else {
+        return {status: 'error'}
+      }
+      
+    },
+    async addPlan() {
+      let temp = JSON.parse(JSON.stringify(this.demoEvents))
+      let asc = {
+          date: this.form.date,
+          title: this.form.title,
+          desc: this.form.desc,
+          isShow: false
+      }
+      temp.splice(0,0,asc)
+      
+      let res = await this.setMemo(temp)
+      console.log('addplan', res)
+      if(res.status=='success') {
         this.form.date = ''
         this.form.title = ''
         this.form.desc = ''
+      }
+      this.addVisible = false
     },
-    delPlan(row, index) {
-        this.demoEvents.splice(index, 1);
+    async delPlan(row, index) {
+      let aa = JSON.parse(JSON.stringify(this.demoEvents))
+      console.log(aa)
+      let newArr = aa.reduce((total, current) => {
+          // ((current.title !== row.title)&&(current.desc!==row.desc)) && total.push(current);
+          current.isShow!==true && total.push(current);
+          return total;
+      }, []);
+      console.log(newArr)
+
+      let res = await this.setMemo(newArr)//更新列表
+      console.log('delplan', res)
+      
+    },
+    arrToStrChange(phoneList) {
+        for(var i=0;i<phoneList.length;i++){
+        var str=""
+        for(var j in phoneList[i]){
+            str+=j+"="+phoneList[i][j]+"|"
+      }
+        str=str.slice(0,-1)
+        phoneList[i]=str
+    }
+      var str1="?"+phoneList.join("&")
+      // console.log(str1)
+      return str1
+     
+    },
+    strToArrChange(str1) {
+       var str1=str1.slice(1);
+      var arr=str1.split("&");
+      var list=[];
+      for(var j=0;j<arr.length;j++){
+          var objArr=arr[j].split("|")
+          var obj={};
+          for(var k=0;k<objArr.length;k++){
+              var objs=objArr[k].split("=");
+              obj[objs[0]]=String(objs[1]).slice(0);
+          }
+          list.push(obj)
+      }
+      // console.log(list)
+      return list
     }
   }
 }
@@ -129,6 +215,9 @@ export default {
     margin-left: 5px;
     color: #F56C6C;
     cursor: pointer;
+}
+.addBoxpen {
+  font-size: 1.5em;
 }
 /* h1, h2, h3 {
   font-weight: normal;
